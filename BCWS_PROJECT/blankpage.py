be4fd89473f2603ad9676ec80f35ac84cd3c14f3
Rhainier Avelino <122976115+RhainierAvelino@users.noreply.github.com>
2024-06-07 14:40:11 +0800

    Add files via upload

---
+++ BCWS_PROJECT/blankpage.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+# Main window
+window = tk.Tk()
+window.geometry("800x550") #changen niyo nalang yung size dito if di kasya
+window.configure(bg="#F0EAE0")
+
+window.resizable(True, True)
+window.mainloop()
